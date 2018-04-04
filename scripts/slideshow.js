e828a2bcbb12951220de12e32da425ec40d29acf
d3sk <rohandavenlol@gmail.com>
2018-04-04 10:15:59 +1000

    Initial commit

---
+++ scripts/slideshow.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.your-class').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
